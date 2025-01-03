1119fb430a24eca56a4ebfd8c9df4cf361bdb497
Jamie Donaldson <jamie.donaldson@mamasandpapas.com>
2025-01-03 12:30:19 +0000

    fixed the code issue

--- plugin.php
+++ plugin.php
@@ -183,28 +183,7 @@ class awxPluginAnsible extends awxPlugin {
 		  $this->api->setAPIResponse('Error','Activity Stream ID is required');
 		}
 	  }
-  
-  
-	  // Get specific Ansible Job Activity Stream
-	  $app->get('/plugin/awx/ansible/jobs/{id}/activity_stream', function ($request, $response, $args) {
-		  $awxPlugin = new awxPluginAnsible();
-		  if ($awxPlugin->auth->checkAccess($awxPlugin->config->get('Plugins','awx')['ACL-JOB'] ?? null)) {
-			  $jobId = $args['id'];
-			  $job = $awxPlugin->GetAnsibleJobActivityStream($jobId);
-			  if ($job) {
-				  $activityStream = $awxPlugin->GetAnsibleActivityStream($job['id'])
-				  if ($activityStream) {
-					  $awxPlugin->api->setAPIResponseData($activityStream);
-				  }
-			  }
-		  }
-		  $response->getBody()->write(jsonE($GLOBALS['api']));
-		  return $response
-			  ->withHeader('Content-Type', 'application/json;charset=UTF-8')
-			  ->withStatus($GLOBALS['responseCode']);
-	});
 
-	
 	public function SubmitAnsibleJob($id,$data) {
 	  $Result = $this->QueryAnsible("post", "job_templates/".$id."/launch/", $data);
 	  if ($Result) {
